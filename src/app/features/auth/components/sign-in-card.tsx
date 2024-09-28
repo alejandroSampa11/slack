fe4d4e76b148ed758e9f6116dc71ee71a9834993
Alejandro Sampablo <alejandro200304@gmail.com>
2024-09-28 16:07:32 -0600

    initial commit

---
+++ src/app/features/auth/components/sign-in-card.tsx
@@ -0,0 +1,81 @@
+import { Button } from '@/components/ui/button'
+import { FaGithub } from 'react-icons/fa'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle} from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Separator } from '@/components/ui/separator'
+import React, { useState } from 'react'
+import { SignInFlow } from '../types'
+import { useAuthActions } from "@convex-dev/auth/react";
+
+interface SignInCardProps{
+    setState: (state: SignInFlow) => void
+}
+
+function SignInCard({setState}: SignInCardProps) {
+    const { signIn } = useAuthActions();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [pending, setPending] = useState(false)
+
+    const handleProviderSignIn = (value: "github")=>{
+        setPending(true);
+        signIn(value)
+        .finally(()=>{
+            setPending(false);
+        })
+    }
+
+  return (
+    <Card className='w-full h-full p-8'>
+        <CardHeader className='px-0 pt-0'>
+            <CardTitle>
+                Login to Continue
+            </CardTitle>
+            <CardDescription>
+                Use your email or another service to continue
+            </CardDescription>
+        </CardHeader>
+        <CardContent className='space-y-5 px-0 pb-0'>
+            <form className='space-y-2.5'>
+                <Input 
+                disabled={false}
+                value= {email}
+                onChange={(e)=>setEmail(e.target.value)}
+                placeholder='Email'
+                type='email'
+                required
+                />
+                <Input 
+                disabled={false}
+                value= {password}
+                onChange={(e)=>setPassword(e.target.value)}
+                placeholder='Password'
+                type='password'
+                required
+                />
+                <Button type='submit' className='w-full' size='lg' disabled={pending}>
+                    Continue
+                </Button>
+            </form>
+            <Separator/>
+                <div className='flex flex-col gap-y-2.5'>
+                    <Button
+                     disabled={pending}
+                     onClick={() => handleProviderSignIn("github")}
+                     variant={'outline'}
+                     size={'lg'}
+                     className='w-full relative'
+                    >
+                        <FaGithub className='size-5 absolute top-3 left-2.5'/>
+                        Continue with Github
+                    </Button>
+                </div>
+                <p className='text-xs text-muted-foreground'>
+                    Don&apos;t have an account? <span onClick={()=> setState("signUp")}className='text-sky-700 hover:underline cursor-pointer'>Sign Up</span>
+                </p>
+        </CardContent>
+    </Card>
+  )
+}
+
+export default SignInCard
