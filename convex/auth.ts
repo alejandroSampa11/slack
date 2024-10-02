ed2399a49679f46b4fa664ec1fec2ca0affbf30b
Alejandro Sampablo <alejandro200304@gmail.com>
2024-10-01 19:47:53 -0600

    Password Authentication

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,6 +1,6 @@
 import GitHub from "@auth/core/providers/github";
 import { convexAuth } from "@convex-dev/auth/server";
- 
+import {Password} from '@convex-dev/auth/providers/Password' 
 export const { auth, signIn, signOut, store } = convexAuth({
-  providers: [GitHub],
+  providers: [Password,GitHub],
 });
--- src/app/features/auth/components/sign-in-card.tsx
+++ src/app/features/auth/components/sign-in-card.tsx
@@ -6,6 +6,7 @@ import { Separator } from '@/components/ui/separator'
 import React, { useState } from 'react'
 import { SignInFlow } from '../types'
 import { useAuthActions } from "@convex-dev/auth/react";
+import {TriangleAlert} from 'lucide-react'
 
 interface SignInCardProps{
     setState: (state: SignInFlow) => void
@@ -13,9 +14,26 @@ interface SignInCardProps{
 
 function SignInCard({setState}: SignInCardProps) {
     const { signIn } = useAuthActions();
+
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
     const [pending, setPending] = useState(false)
+
+    const onPasswordSignIn = (e: React.FormEvent<HTMLFormElement>) =>{
+        e.preventDefault();
+        setPending(true);
+        signIn("password",{email, password, flow: "signIn"})
+        .then(() => {
+            window.location.href = "/";
+        })
+        .catch(() => {
+            setError('Invalid email or password');
+        })
+        .finally(() => {
+            setPending(false);
+        });
+    }
 
     const handleProviderSignIn = (value: "github")=>{
         setPending(true);
@@ -35,8 +53,14 @@ function SignInCard({setState}: SignInCardProps) {
                 Use your email or another service to continue
             </CardDescription>
         </CardHeader>
+        {!!error && (
+            <div className='bg-destructive/15 p-3 rounded-md flex items-center gap-x-2 text-sm text-destructive mb-6'>
+                <TriangleAlert className='size-4'/>
+                <p>{error}</p>
+            </div>
+        )}
         <CardContent className='space-y-5 px-0 pb-0'>
-            <form className='space-y-2.5'>
+            <form onSubmit={onPasswordSignIn} className='space-y-2.5'>
                 <Input 
                 disabled={false}
                 value= {email}
