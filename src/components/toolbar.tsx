df65d5c0fb30fd1ab6b5c4ad9d64b439b6991000
Alejandro Sampablo <alejandro200304@gmail.com>
2025-01-12 14:06:12 -0600

    Message Toolbar

---
+++ src/components/toolbar.tsx
@@ -0,0 +1,72 @@
+import React from 'react'
+import { Id } from '../../convex/_generated/dataModel';
+import { Button } from './ui/button';
+import { MessageSquareText, Pencil, Smile, Trash } from 'lucide-react';
+import Hint from './hint';
+import EmojiPopover from './emoji-popover';
+
+interface ToolbarProps {
+    isAuthor: boolean;
+    isPending: boolean;
+    handleEdit: (id: Id<"messages"> | null) => void;
+    handleThread: () => void;
+    handleDelete: () => void;
+    handleReaction: (value: string) => void
+    hideThreadButton?: boolean;
+}
+
+function Toolbar({ isAuthor, isPending, handleEdit, handleThread, handleDelete, handleReaction, hideThreadButton }: ToolbarProps) {
+    return (
+        <div className='absolute top-0 right-5'>
+            <div className='group-hover:opacity-100 opacity-0 transition-opacity border bg-white rounded-md shadow-sm'>
+                <EmojiPopover
+                    hint='Add Reaction'
+                    onEmojiSelect={(emoji) => handleReaction(emoji.native)}
+                >
+                    <Button
+                        variant="ghost"
+                        size="iconSm"
+                        disabled={isPending}
+                    >
+                        <Smile className='size-4' />
+                    </Button>
+                </EmojiPopover>
+                {!hideThreadButton && (
+                    <Hint label='Reply in Thread'>
+                        <Button
+                            variant="ghost"
+                            size="iconSm"
+                            disabled={isPending}
+                        >
+                            <MessageSquareText className='size-4' />
+                        </Button>
+                    </Hint>
+                )}
+                {isAuthor && (
+                    <>
+                        <Hint label='Edit Message'>
+                            <Button
+                                variant="ghost"
+                                size="iconSm"
+                                disabled={isPending}
+                            >
+                                <Pencil className='size-4' />
+                            </Button>
+                        </Hint>
+                        <Hint label='Delete Message'>
+                            <Button
+                                variant="ghost"
+                                size="iconSm"
+                                disabled={isPending}
+                            >
+                                <Trash className='size-4' />
+                            </Button>
+                        </Hint>
+                    </>
+                )}
+            </div>
+        </div >
+    )
+}
+
+export default Toolbar
